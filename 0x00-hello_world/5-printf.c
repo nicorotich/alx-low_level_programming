76bf1d5c1bb3f691e724ac0b47906cf9561ea437
nicorotich <nicholaskrotich8@gmail.com>
2021-02-04 13:24:38 +0300

    use printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@void: no arguments
+ *Description: the program starts and ends here.
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
