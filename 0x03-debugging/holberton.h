318ccb064e0932eb7daefb7cc54fff9cac675a5c
nicorotich <nicholaskrotich8@gmail.com>
2021-02-12 03:26:06 +0300

    Initial commit

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int 1);
-int largest_number(inta, int b, int c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
