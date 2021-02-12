6f2b84e8cd01c02fc68094d2d643f0d251c3b6cd
nicorotich <nicholaskrotich8@gmail.com>
2021-02-12 19:05:21 +0300

    Initial commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,29 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 99; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
 		else if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			printf("%s", "Fizz");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("%s", "Buzz");
+		}
 		else
-			print("%i ", i);
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
-	printf("Buzz\n");
+	printf("\n");
 	return (0);
 }
